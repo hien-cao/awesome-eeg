3dbf33b1a5e51b732a5c0afdf2510c0b298b1ad7
Hien Cao <hien.cao@aalto.fi>
2019-04-03 00:32:47 +0300

    first deployment: no webpack

--- public/index.js
+++ public/index.js
@@ -1,8 +1,8 @@
 import { ImageService } from './js/image.service';
 import { SoundService } from './js/sound.service';
 import { ComponentService } from './js/component.service';
-import { run } from './js/app';
-import './css/main.scss';
+import { run } from './js/game';
+// import './css/main.scss';
 
 if (window.location.pathname == '/flappy') {
 	const componentService = new ComponentService();
